effbdd4b9157b9da943fb3247fffaa5525dec63f
Saketh Pachika <53295465+sakethpachika@users.noreply.github.com>
2022-04-23 17:46:56 -0500

    Add files via upload

---
+++ Two Sum.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        emp = {}
+        for i,j in enumerate(nums):
+            diff = target - j
+            if diff in emp:
+                return [emp[diff],i]
+            emp[j]=i
